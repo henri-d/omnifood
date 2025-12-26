d9f525cdfc478326b8caa9ef570c1923ff9480ea
henri-d <43344612+henri-d@users.noreply.github.com>
2025-12-26 11:28:11 +0700

    sticky navigation

--- js/script.js
+++ js/script.js
@@ -58,6 +58,32 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.querySelector("body").classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.querySelector("body").classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
